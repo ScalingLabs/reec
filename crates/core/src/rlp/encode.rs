f561f33b23711e949bfac9894358e7933a9ecfd6
Owanikin <oderindeife@gmail.com>
2025-01-30 16:19:35 +0100

    simple RLP encode implementation: u16, u32, u64, (), usize

--- crates/core/src/rlp/encode.rs
+++ crates/core/src/rlp/encode.rs
@@ -32,6 +32,7 @@ impl RLPEncode for bool {
 impl RLPEncode for u8 {
     fn encode(&self, buf: &mut dyn BufMut) {
         match *self {
+            0 => buf.put_u8(0x80),
             n @ 1..=0x7f => buf.put_u8(n),
             n => {
                 let mut bytes = ArrayVec::<[u8; 8]>::new();
@@ -42,5 +43,79 @@ impl RLPEncode for u8 {
                 buf.put_slice(&bytes[start..]);
             }
         }
+    }
+}
+
+impl RLPEncode for u16 {
+    fn encode(&self, buf: &mut dyn BufMut) {
+        match *self {
+            0 => buf.put_u8(0x80),
+            n@ 1..=0x7f => buf.put_u8(n as u8),
+            n => {
+                let mut bytes = ArrayVec::<[u8; 8]>::new();
+                bytes.extend_from_slice(&n.to_be_bytes());
+                let start = bytes.iter().position(|&x| x != 0).unwrap();
+                let len = bytes.len() - start;
+                buf.put_u8(0x80 + len as u8);
+                buf.put_slice(&bytes[start..]);
+            }
+        }
+    }
+}
+
+impl RLPEncode for u32 {
+    fn encode(&self, buf: &mut dyn BufMut) {
+        match *self {
+            0 => buf.put_u8(0x80),
+            n @ 1..0x7f => buf.put_u8(n),
+            n => {
+                let mut bytes = ArrayVec::<[u8; 8]>::new();
+                bytes.extend_from_slice(&n.to_be_bytes());
+                let start = bytes.iter().position(|&x| x != 0).unwrap();
+                let len = bytes.len() - start;
+                buf.put_u8(0x80 + len as u8);
+                buf.put_slice(&bytes[start..]);
+            }
+        }
+    }
+}
+
+impl RLPEncode for u64 {
+    fn encode(&self, buf: &mut dyn BufMut) {
+        match *self {
+            0 => buf.put_u8(0x80),
+            n @ 1..0x7f => buf.put_u8(n as u8),
+            n => {
+                let mut bytes = ArrayVec::<[u8; 8]>::new();
+                bytes.extend_from_slice(&n.to_be_bytes());
+                let start = bytes.iter().position(|&x| x != 0).unwrap();
+                let len = bytes.len() - start;
+                buf.put_u8(0x80 + len as u8);
+                buf.put_slice(&bytes[start..]);
+            }
+        }
+    }
+}
+
+impl RLPEncode for usize {
+    fn encode(&self, buf: &mut dyn BufMut) {
+        match *self {
+            0 => buf.put_u8(0x80),
+            n @ 1..=0x7f => buf.put_u8(n as u8),
+            n => {
+                let mut bytes = ArrayVec::<[u8; 8]>::new();
+                bytes.extend_from_slice(&n.to_be_bytes());
+                let start = bytes.iter().position(|&x|x != 0).unwrap();
+                let len = bytes.len() - start;
+                buf.put_u8(0x80 + len as u8);
+                buf.put_slice(&bytes[start..]);
+            }
+        }
+    }
+}
+
+impl RLPEncode for () {
+    fn encode(&self, buf: &mut dyn BufMut) {
+        buf.put_u8(0x80);
     }
 }
