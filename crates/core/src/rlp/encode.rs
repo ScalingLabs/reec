3580ba660595d1ba66e92b63aa68204ee919a190
Owanikin <oderindeife@gmail.com>
2025-02-03 17:03:11 +0100

    simple RLP encode implementation: bytes

--- crates/core/src/rlp/encode.rs
+++ crates/core/src/rlp/encode.rs
@@ -67,7 +67,7 @@ impl RLPEncode for u32 {
     fn encode(&self, buf: &mut dyn BufMut) {
         match *self {
             0 => buf.put_u8(0x80),
-            n @ 1..0x7f => buf.put_u8(n),
+            n @ 1..0x7f => buf.put_u8(n as u8),
             n => {
                 let mut bytes = ArrayVec::<[u8; 8]>::new();
                 bytes.extend_from_slice(&n.to_be_bytes());
@@ -117,5 +117,98 @@ impl RLPEncode for usize {
 impl RLPEncode for () {
     fn encode(&self, buf: &mut dyn BufMut) {
         buf.put_u8(0x80);
+    }
+}
+
+impl RLPEncode for [u8] {
+    #[inline(always)]
+    fn encode(&self, buf: &mut dyn BufMut) {
+        if self.len() == 1 && self[0] < 0x80 {
+            buf.put_u8(self[0]);
+        } else {
+            let len = self.len();
+            if len < 56 {
+                buf.put_u8(0x80 + len as u8);
+            } else {
+                let mut bytes = ArrayVec::<[u8; 8]>::new();
+                bytes.extend_from_slice(&len.to_be_bytes());
+                let start = bytes.iter().position(|&x|x != 0).unwrap();
+                let len = bytes.len() - start;
+                buf.put_u8(0x7f + len as u8);
+                buf.put_slice(&bytes[start..]);
+            }
+            buf.put_slice(self);
+        }
+    }
+}
+
+impl <const N: usize> RLPEncode for [u8; N] {
+    fn encode(&self, buf: &mut dyn BufMut) {
+        self.as_ref().encode(buf);
+    }
+}
+
+impl RLPEncode for str {
+    fn encode(&self, buf: &mut dyn BufMut) {
+        self.as_bytes().encode(buf);
+    }
+}
+
+impl RLPEncode for &str {
+    fn encode(&self, buf: &mut dyn BufMut) {
+        self.as_bytes().encode(buf);
+    }
+}
+
+impl RLPEncode for String {
+    fn encode(&self, buf: &mut dyn BufMut) {
+        self.as_bytes().encode(buf);
+    }
+}
+
+impl <T: RLPEncode> RLPEncode for Vec<T> {
+    fn encode(&self, buf: &mut dyn BufMut) {
+        if self.is_empty() {
+            buf.put_u8(0xc0);
+        } else {
+            let mut total_len = 0;
+            for item in self {
+                total_len += item.length();
+            }
+            if total_len < 56 {
+                buf.put_u8(0xc0 + total_len as u8);
+            } else {
+                let mut bytes = ArrayVec::<[u8; 8]>::new();
+                bytes.extend_from_slice(&total_len.to_be_bytes());
+                let start = bytes.iter().position(|&x| x!= 0).unwrap();
+                let len = bytes.len() - start;
+                buf.put_u8(0x7f + len as u8);
+                buf.put_slice(&bytes[start..]);
+            }
+            for item in self {
+                item.encode(buf);
+            }
+        }
+    }
+}
+
+impl RLPEncode for Ipv4Addr {
+    fn encode(&self, buf: &mut dyn BufMut) {
+        self.octets().encode(buf);
+    }
+}
+
+impl RLPEncode for Ipv6Addr {
+    fn encode(&self, buf: &mut dyn BufMut) {
+        self.octets().encode(buf);
+    }
+}
+
+impl RLPEncode for IpAddr {
+    fn encode(&self, buf: &mut dyn BufMut) {
+        match self {
+            IpAddr::V4(ip) => ip.encode(buf),
+            IpAddr::V6(ip) => ip.encode(buf),
+        }
     }
 }
