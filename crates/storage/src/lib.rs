dc6018d29796d2a31a6de4e70a71490a62414cc3
Owanikin <oderindeife@gmail.com>
2025-02-12 15:31:20 +0100

    test: storage mdbx struct smoke test

--- crates/storage/src/lib.rs
+++ crates/storage/src/lib.rs
@@ -27,8 +27,6 @@ mod tests {
         table_info
     };
 
-    use crate::Example;
-
     #[test]
     fn mdbx_smoke_test() {
         // Declare tables used for the smoke test
@@ -57,7 +55,73 @@ mod tests {
             txn.get::<Example>(key).unwrap()
         };
         assert_eq!(read_value, Some(value));
-        
 
+    }
+
+    #[test]
+    fn mdbx_struct_smoke_test() {
+        #[derive(Clone, Copy, Debug, PartialEq, Eq)]
+        pub struct ExampleKey ([u8; 32]);
+
+        impl Encodable for ExampleKey {
+            type Encoded = [u8; 32];
+
+            fn encode(self) -> Self::Encoded {
+                Encodable::encode(self.0)
+            }
+        }
+
+        #[derive(Clone, Copy, Debug, PartialEq, Eq)]
+    pub struct ExampleValue{
+        x: u64,
+        y: [u8; 32],
+    }
+
+    impl Encodable for ExampleValue {
+        type Encoded = [u8; 40];
+
+        fn encode(self) -> Self::Encoded {
+            let mut encoded = [0u8; 40];
+
+            encoded[..8].copy_from_slice(&self.x.to_ne_bytes());
+            encoded[8..].copy_from_slice(&self.y);
+            encoded
+        }
+    }
+
+    impl Decodable for ExampleValue {
+        fn decode(b: &[u8]) -> anyhow::Result<Self> {
+            let x = u64::from_ne_bytes(b[..8].try_into()?);
+            let y = b[8..].try_into()?;
+            Ok(Self { x, y })
+        }
+    }
+
+    // Declare tables used fro the smoke test
+    table!(
+        /// Example table
+        ( StructsExample) ExampleKey => ExampleValue
+    );
+
+    // Assemble databse chart
+    let tables = [table_info!(StructsExample)].into_iter().collect();
+    let key = ExampleKey([151; 32]);
+    let value = ExampleValue{ x: 42, y: [42; 32]};
+    let db = Database::create(None, &tables).unwrap();
+
+    // Write Values
+    {
+        let txn = db.begin_readwrite().unwrap();
+        txn.upsert::<StructsExample>(key, value).unwrap();
+        txn.commit().unwrap();
+    }
+
+    // Read written values
+    let read_value = {
+        let txn = db.begin_read().unwrap();
+        txn.get::<StructsExample>(key).unwrap()
+    };
+
+    assert_eq!(read_value, Some(value));
     }
 }
