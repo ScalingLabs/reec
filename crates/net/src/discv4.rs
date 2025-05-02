9754e37c1c7296c6ec76b989ac841fa459688d49
Owanikin <oderindeife@gmail.com>
2025-05-02 22:36:18 +0100

    feat: start an RLPx connection

--- crates/net/src/discv4.rs
+++ crates/net/src/discv4.rs
@@ -9,6 +9,7 @@ use reec_core::rlp::{
 };
 use reec_core::{H256, H512, H520};
 use k256::ecdsa::SigningKey;
+use sha3::{Digest, Keccak256};
 
 const MAX_NODE_RECORD_ENCODED_SIZE: usize = 300;
 
@@ -74,15 +75,15 @@ impl Message {
         self.encode_with_type(&mut data);
 
 
-        let digest = keccak_hash::keccak_buffer(&mut &data[signature_size..]).unwrap();
-        let (signature, recovery_id) = node_signer.sign_prehash_recoverable(&digest.0).expect("failed to sign");
+        let digest = Keccak256::digest(&data[signature_size..]);
+        let (signature, recovery_id) = node_signer.sign_prehash_recoverable(&digest).expect("failed to sign");
         let b = signature.to_bytes();
 
         data[..signature_size - 1].copy_from_slice(&b);
         data[signature_size - 1] = recovery_id.to_byte();
 
-        let hash = keccak_hash::keccak_buffer(&mut &data[..]).unwrap();
-        buf.put_slice(&hash.0);
+        let hash = Keccak256::digest(&data[..]);
+        buf.put_slice(&hash);
         buf.put_slice(&data[..]);
     }
 
@@ -536,8 +537,7 @@ impl RLPEncode for Node {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use reec_core::H264;
-    use keccak_hash::H256;
+    use reec_core::{H256, H264};
     use std::str::FromStr;
     use std::fmt::Write;
     use std::net::Ipv4Addr;
