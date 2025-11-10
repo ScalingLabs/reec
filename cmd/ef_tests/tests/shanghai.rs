4abc97cbde309453cd5f34dd4d62b474e73f3633
Owanikin <oderindeife@gmail.com>
2025-11-10 17:28:55 +0100

    fix: timestamp check to greater or equal than 15000

--- cmd/ef_tests/tests/shanghai.rs
+++ cmd/ef_tests/tests/shanghai.rs
@@ -29,5 +29,5 @@ datatest_stable::harness!(
     r"^.*/*",
     parse_and_execute,
     "vectors/shanghai/eip4895_withdrawals/",
-    r"^(?!.*withdrawals/self_destructing_account\.json$).*"
+    r"^.*/*"
 );
