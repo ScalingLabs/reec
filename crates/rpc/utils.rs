5e8b6de6c70b30bc6c82d925404deb6d4fc397eb
Owanikin <oderindeife@gmail.com>
2025-12-17 11:58:10 +0100

    fix: error code for invalid params rpc error

--- crates/rpc/utils.rs
+++ crates/rpc/utils.rs
@@ -19,7 +19,7 @@ impl From<RpcErr> for RpcErrorMetadata {
                 message: "Method Not Found".to_string(),
             },
             RpcErr::BadParams => RpcErrorMetadata {
-                code: -32602,
+                code: -32000,
                 message: "Invalid params".to_string(),
             },
             RpcErr::UnsuportedFork => RpcErrorMetadata { 
