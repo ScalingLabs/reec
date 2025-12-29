d45ae35195d8174ba43dddcc109ba7e0947125bf
Owanikin <oderindeife@gmail.com>
2025-12-29 13:19:25 +0100

    fix: declare BlockTotalDifficultie table in init_db

--- crates/storage/engines/libmdbx.rs
+++ crates/storage/engines/libmdbx.rs
@@ -488,6 +488,7 @@ impl Encodable for ChainDataIndex {
 pub fn init_db(path: Option<impl AsRef<Path>>) -> Database {
     let tables = [
         table_info!(BlockNumbers),
+        table_info!(BlockTotalDifficulties),
         table_info!(Headers),
         table_info!(Bodies),
         table_info!(AccountInfos),
